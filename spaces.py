2c49b048ff1790d01e0ef72601e27aa602b56800
Jacek Spera <jacekspera@gmail.com>
2014-12-09 09:06:54 +0000

    added fifo communication

--- spaces.py
+++ spaces.py
@@ -6,15 +6,16 @@ import ipdb
 
 reserved_option_names = ['_uses', '_provider']
 
-
+def get_config(filepath):
+    cfg = config.SpacesConfigParser(allow_no_value=True)
+    cfg.readfp(open(filepath))
+    return cfg
 
 def sort_sections(config):
     graph = {}
     for section in config.sections():
         graph[section] = config.getuses(section)
     return toposort.toposort_flatten(graph)
-
-
 
 def get_providers(config):
     _providers = []
@@ -30,10 +31,82 @@ def get_providers(config):
                 params[opt] = val
         _providers.append(provider(params))
     return _providers
-        
+
+def send_error(pipe, error):
+    with open(pipe, 'w') as stream:
+        print "Sending: %s" % error
+        stream.write('ERR %s' % error)
+
+def send_end(pipe):
+    with open(pipe, 'w') as stream:
+        print "Sending: END"
+        stream.write('END')
+
+def send_command(pipe, command):
+    with open(pipe, 'w') as stream:
+        print "Sending: CMD %s" % command
+        stream.write('CMD %s' % command)
+
+def send_info(pipe, info):
+    with open(pipe, 'w') as stream:
+        print "Sending: INF %s" % info
+        stream.write('INF %s' % info)
+
+def receive_command(pipe):
+    with open(pipe, 'r') as stream:
+        data = stream.readline()[:-1]
+        if not data.startswith('CMD ') or data[4:] not in ['provide', 'revert']:
+            pass  #TODO handle error
+        print "Received: %s" % data
+        return data[4:]
+
+def receive_status(pipe):
+    with open(pipe, 'r') as stream:
+        data = stream.readline()[:-1]
+        if not data.startswith('XST '):
+            pass  #TODO handle error
+        print "Received: %s" % data
+        return int(data[4:])
+
+def receive_stdout(pipe):
+    with open(pipe, 'r') as stream:
+        data = stream.readline()[:-1]
+        if not data.startswith('STO '):
+            pass  #TODO handle error
+        print "Received: %s" % data
+        return data[4:]
+
+def receive_stderr(pipe):
+    with open(pipe, 'r') as stream:
+        data = stream.readline()[:-1]
+        if not data.startswith('STE '):
+            pass  #TODO handle error
+        print "Received: %s" % data
+        return data[4:]
 
 if __name__ == "__main__":
-    file_name = sys.argv[1]
-    _config = config.SpacesConfigParser(allow_no_value=True)
-    _config.readfp(open(sys.argv[1]))
-    print get_providers(_config)
+    cfg_file = sys.argv[1]
+    in_pipe = sys.argv[2]
+    out_pipe = sys.argv[3]
+
+    providers = get_providers(get_config(cfg_file))
+    space_cmd = receive_command(in_pipe)
+    for provider in providers:
+        fn = getattr(provider, space_cmd)
+        send_info(out_pipe, fn.__doc__)
+        cmd_gen = fn()
+        # first command goes without passing data back
+        send_command(out_pipe, cmd_gen.next())
+        while True:
+            rcode = receive_status(in_pipe)
+            stdout = receive_stdout(in_pipe)
+            stderr = receive_stderr(in_pipe)
+            try:
+                send_command(out_pipe, cmd_gen.send((rcode, stdout, stderr)))
+            except StopIteration:
+                #TODO handle last rcode, stdout, stderr
+                break
+            
+    send_end(out_pipe)
+
+
